1917419c4a9d285d931488794d3f8a1814fb2017
SedraRan19 <sedra@alsasoft.fr>
2024-02-11 13:41:31 +0300

    curd total service

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule, NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { DemoFlexyModule } from 'src/app/demo-flexy-module';
+import { ServicesService } from './services.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 interface cards {
   image: string;
@@ -11,15 +14,96 @@ interface cards {
   selector: 'app-services',
   standalone: true,
   templateUrl: './services.component.html',
-  imports: [DemoFlexyModule,CommonModule,NgClass],
+  imports: [DemoFlexyModule,CommonModule,NgClass,HttpClientModule,FormsModule],
   styleUrls: ['./services.component.scss']
 })
 
 export class ServicesComponent  {
-
   isVisibleFrom:boolean = false;
+  services: any[] = [];
+  newService = {
+    name: '',
+    duration: 0,
+    price: 0,
+    commissionPercentage: 0,
+    description: '',
+  };
+  editingService: any = null;
+
+  constructor(private servicesService: ServicesService){}
+
+  ngOnInit(): void{
+    this.loadServices();
+  }
+
+  loadServices() {
+    this.servicesService.getAllServices().subscribe((services) => {
+      this.services = services;
+      console.log(services);
+    });
+  }
+
+  onSubmit(){
+    if(this.editingService){
+      this.servicesService.updateService(this.editingService._id,this.newService).subscribe(
+        (response)=>{
+          this.loadServices();
+          console.log('Service modifié avec succès :',response);
+        },
+        (error)=>{
+          console.log('Erreur lors de la modification du service :',error);
+        }
+      );
+      this.newService = {
+        name: '',
+        duration: 0,
+        price: 0,
+        commissionPercentage: 0,
+        description: '',
+      };
+      this.editingService = null;
+      this.isVisibleFrom = false;
+    }
+    else{
+      this.servicesService.createService(this.newService).subscribe(
+        (response) => {
+          this.loadServices();
+          console.log('Service créé avec succès :', response);
+        },
+        (error) => {
+          console.error('Erreur lors de la création du service :', error);
+        }
+      );
+      this.isVisibleFrom = false;
+    }
+  }
+
+  onDelete(serviceId: string){
+    this.servicesService.deleteService(serviceId).subscribe(
+    (response)=>{
+      console.log('Service supprimé avec succès :', response);
+      this.loadServices();  
+    },
+    (error)=>{
+      console.error('Erreur lors de la suppression du service :', error);
+    }
+    )
+  };
+
+  onEdit(service: any) {
+    this.newService = { ...service };// Mettez à jour le formulaire avec les valeurs du service en cours d'édition
+    this.editingService = service;
+    this.isVisibleFrom = true;
+  }
 
   openNewService(){
+    this.newService = {
+      name: '',
+      duration: 0,
+      price: 0,
+      commissionPercentage: 0,
+      description: '',
+    };
     this.isVisibleFrom = true;
   }
 
@@ -31,18 +115,6 @@ export class ServicesComponent  {
     {
       image: "assets/images/u2.webp",
       btn: "warn",
-    },
-    {
-      image: "assets/images/u3.webp",
-      btn: "primary",
-    },
-    {
-      image: "assets/images/u4.webp",
-      btn: "accent",
-    },
-    {
-      image: "assets/images/u4.webp",
-      btn: "accent",
     },
   ]
 }
